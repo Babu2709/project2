38bb0913cba982007ae45ad69598e51665fd6c72
Babu2709 <muntimadagunagendrababu35@gmail.com>
2023-11-10 12:55:11 +0530

    first commit

---
+++ App2/views.py
@@ -0,0 +1,9 @@
+from django.shortcuts import render
+
+# Create your views here.
+def nagendra(request):
+    return render(request,'nagendra.html')
+def baby(request):
+    return render(request,'baby.html')
+def babu(request):
+    return render(request,'babu.html')
